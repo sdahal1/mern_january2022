fffd81fb6cb6b27a38b9786f2eee8f812181a088
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-01-12 14:18:03 -0800

    finished class components

--- 03-react_intro_and_class_components/03-class-components-menu-demo/src/App.js
+++ 03-react_intro_and_class_components/03-class-components-menu-demo/src/App.js
@@ -9,14 +9,25 @@ function App() {
      <h1>Dojo Diner- The finest food of the JavaScript Developers</h1>
 
      {/* information you pass down to a component is called props  */}
-     <MenuItem price = {15.99} dish = {"Spicy Salmon Roll"} ></MenuItem>
+     <MenuItem price = {15.99} dish = {"Spicy Salmon Roll"} likes = {2} >
+        <p>Its really spicy and inflationy</p>
+        <em>Allegies Info: Its gluten free, yay. Extra 5 dollars tho'</em>
+     </MenuItem>
      <hr />
-     <MenuItem price = {12.99} dish = {"Squash"}></MenuItem>
+     <MenuItem price = {12.99} dish = {"Squash"} likes = {10}>
+       <p>Its just a squash. Saschquash tho'</p>
+     </MenuItem>
       <hr />
-      <MenuItem price = {8.99} dish = {"Crab Rangoon"} ></MenuItem>
+      <MenuItem price = {8.99} dish = {"Crab Rangoon"} likes = {0} >
+        <p>This is bougie sounding and it tastes good too</p>
+      </MenuItem>
       <hr />
-      <MenuItem price = {20.99} dish = {"Momo"}></MenuItem>
+      <MenuItem price = {20.99} dish = {"Momo"} likes = {5}> 
+        <p>This is an authentic nepali dumpling. Namaste</p>
+      </MenuItem>
       <hr />
+
+      
     </div>
   );
 }
--- 03-react_intro_and_class_components/03-class-components-menu-demo/src/components/MenuItem.jsx
+++ 03-react_intro_and_class_components/03-class-components-menu-demo/src/components/MenuItem.jsx
@@ -1,15 +1,52 @@
+/*
+Difference between props vs state:
+1. Props are information that we pass down to a component when we render it (load up the component with initial information). Props are not supposed to be changeable. If you want to make information that comes from props changeable, you have to put it in state. 
+
+2. State is information that can change within a component and uniquely to a specific component, like the likes in this application. Example- weather can change from region to region. State variables can change from component to component. One state variabel's likes might go up by 1, another might stay the same. 
+
+*/
+
+
+
 import React, { Component } from 'react'; //importing React and Component so that we can create React component
 
 class MenuItem extends Component {
+    constructor(props) {
+        super(props);
+
+        
+        this.state = {
+            likes: this.props.likes
+        };
+    }
+
+
     render() {
-        console.log("logging this.props", this.props)
+
+        let clickHandler = ()=>{
+            console.log("hi");
+            //when the button is clicked, increase the state variable for likes
+
+            //this.state.likes++; //this way does not work even though it would make sense. State variables wont change like a normal variable
+
+
+            //this is how you change a state variablee-> use the setState() functionality
+            this.setState({likes: this.state.likes+1})
+
+            // alert("you clicked it! yayy!")
+        }
+
+
+        // console.log("logging this.props", this.props)
         let {dish, price} = this.props
 
         return <div> 
             <h1>{dish}</h1>
             <p>Price: {price} (Inflation doe)</p>
-            <p>Description: Its good but inflation tho'</p>
-            <p>Number of likes: 2</p>
+            {this.props.children}
+            <p>Number of likes: {this.state.likes} </p>
+            <button className= "btn btn-primary" onClick = {clickHandler}>Like {this.props.dish}</button>
+
         </div>
         
     }
