4c72942158398ea2925205f1733afc0024605461
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-01-14 09:06:41 -0800

    form created

--- 04-react_functional_components/03-wall-of-ninjas/src/components/Ninja.jsx
+++ 04-react_functional_components/03-wall-of-ninjas/src/components/Ninja.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Ninja =()=>{
-
+const Ninja = (props) =>{
+    return (
+        <form>
+            <div className="form-group">
+                <label htmlFor="">Ninja name:</label>
+                <input type="text" name="" id="" className="form-control" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="">Image Url:</label>
+                <input type="text" name="" id="" className="form-control" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="">Favorite Color:</label>
+                <input type="text" name="" id="" className="form-control" />
+            </div>
+            <input type="submit" value="Create Ninja!" />
+        </form>
+    )
 }
 
 
