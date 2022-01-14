742a82315a780f690990f400077ce60d31678217
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-01-14 09:08:50 -0800

    form created

--- 04-react_functional_components/03-wall-of-ninjas/src/App.js
+++ 04-react_functional_components/03-wall-of-ninjas/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Ninja from './components/Ninja';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <h1>The Wall of Ninjas</h1>
+      <Ninja></Ninja>
     </div>
   );
 }
