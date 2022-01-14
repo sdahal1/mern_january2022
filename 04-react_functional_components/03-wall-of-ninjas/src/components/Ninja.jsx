aee9ee282c65ba25cf35c244c431e71a44af4b26
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-01-14 13:44:04 -0800

    completed extra callback parameters

--- 04-react_functional_components/03-wall-of-ninjas/src/components/Ninja.jsx
+++ 04-react_functional_components/03-wall-of-ninjas/src/components/Ninja.jsx
@@ -5,21 +5,50 @@ const Ninja = (props) =>{
     let [ninjaName, setNinjaName ] = useState("");
     let [imageUrl, setImageUrl ] = useState("");
     let [favColor, setFavColor ] = useState("");
+    
+    //this state variable to toggle their graduation status
+    let [graduated, setGraduated] = useState(false)
+
 
     // let listOfNinjas = [];
     //create a state variable to store every ninjas that was submitted from the from
     let [listOfNinjas, setListOfNinjas] = useState([]);
 
+
+    //this function runs when we submit the form
     const createNinja = (e)=>{
         e.preventDefault(); //the default behavior of a form when submitting is to reload the page and we are prventing that from happening b/c we don't want the page to reload
         console.log("submitted the form!")
         console.log(ninjaName,imageUrl, favColor)
-        let ninjaObj = {ninjaName, imageUrl, favColor}
+        let ninjaObj = {ninjaName, imageUrl, favColor, graduated}
         console.log(ninjaObj);
+        
 
         setListOfNinjas([...listOfNinjas,ninjaObj])
 
     }
+
+
+    //toggle graduation for student function that gets called when the checkbox is clicked
+    const toggleGraduation = (idx)=>{
+        console.log("trying to graduate this student-> logging idx of student here: ", idx)
+
+        //create a copy of list of ninjas and update the copy list of ninjas array at specific index of student we want to graduate to have their graduation property === true
+        let [...copyOfListOfNinjas] = listOfNinjas;
+        console.log("here is the list of ninjas copy->",copyOfListOfNinjas )
+
+        copyOfListOfNinjas[idx].graduated = !copyOfListOfNinjas[idx].graduated  
+
+        console.log("here is the list of ninjas copy after updating it->",copyOfListOfNinjas )
+
+        //update the state with this newly updated information
+        setListOfNinjas(copyOfListOfNinjas);
+
+        // setListOfNinjas(listOfNinjas)
+    
+    }
+
+    
 
 
     return (
@@ -31,7 +60,7 @@ const Ninja = (props) =>{
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Image Url:</label>
-                    <input type="text" name="" id="" className="form-control" onChange = {(e)=>setImageUrl(e.target.value)} />
+                    <input type="text" name="imageUrl" id="" className="form-control" onChange = {(e)=>setImageUrl(e.target.value)} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Favorite Color:</label>
@@ -43,12 +72,13 @@ const Ninja = (props) =>{
             <h3>Here are our list of ninjas!</h3>
 
             {
-                listOfNinjas.map((ninja)=>{
+                listOfNinjas.map((ninja, i)=>{
                     return (
-                        <div style = {{border: "1px solid black", backgroundColor: ninja.favColor, display: "inline-block"}}>
-                            <h1>{ninja.ninjaName}</h1>
+                        <div key = {i} style = {{border: "1px solid black", backgroundColor: ninja.favColor, display: "inline-block", textDecoration: ninja.graduated? 'line-through': 'none'}}>
+                            <h1>{ninja.ninjaName}- Idx # {i}</h1>
                             <p><img src={ninja.imageUrl} alt="" width= "100px"/></p>
                             <p>Favorite color: {ninja.favColor}</p>
+                            <p><input type="checkbox" name="" id="" onClick = {(e)=>toggleGraduation(i)}/>Graduate</p>
                         </div>
                     )
                 })
