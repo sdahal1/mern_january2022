69d9e4b7056b6fc421d08d75412a8bbca4448a35
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-01-18 08:47:36 -0800

    crypto api lecture

--- 05-react_apis/crypto-api/src/App.js
+++ 05-react_apis/crypto-api/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import CryptoCoinsUsingFetch from './components/CryptoCoinsUsingFetch';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Crypto Api demo</h1>
+      <CryptoCoinsUsingFetch></CryptoCoinsUsingFetch>
     </div>
   );
 }
---
+++ 05-react_apis/crypto-api/src/components/CryptoCoinsUsingFetch.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CryptoCoinsUsingFetch = ()=>{
+    return (
+        <h3>Hello from crypto component</h3>
+    )
+}
+
+
+export default CryptoCoinsUsingFetch;
