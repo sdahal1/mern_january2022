d9090043b6236c82ced290f56692c6eea4f57b0c
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-01-21 09:08:20 -0800

    express

--- 07-express/01-quotes-api-express/server.js
+++ 07-express/01-quotes-api-express/server.js
@@ -3,6 +3,27 @@ const app = express(); //create a variable called app--> this variable is an ins
 const port = 8000;
 
 
+//this is what you did in flask
+// @app.route("/hello")
+// def sayhello():
+//     return "hello"
+
+let quotes = [
+    {content: "It is not the mountains ahead that will wear you out, it is the pebble in your shoe", author:"Muhammad Ali"},
+    {content:"A wise man once said nothing at all", author:"Wes"},
+    {content:"Comparison is a theif of joy", author:"Theodore Roosevelt"},
+    {content:"Fall down 7 times stand up 8", author:"Japanese Proverb"},
+    {content:"You Attract what you vibrate", author:"Fritz"},
+    {content:"Wherever you go, there you are", author:"Eckhart Tolle"}
+]
+
+
+//this is an api endpoint
+app.get("/api/hello",(req,res)=>{
+    res.json({msg:"hello express!!!"})
+} )
+
+
 
 
 
