227769ad3cfa41c1528d4f7f1d062665b3af4348
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-01-25 09:01:19 -0800

    mongoose set up models

--- 08-mongodb_mongoose/01-ninjas_belt_reviewer/server/routes/ninja.routes.js
+++ 08-mongodb_mongoose/01-ninjas_belt_reviewer/server/routes/ninja.routes.js
@@ -7,17 +7,24 @@ const Ninja = require("../models/ninja.model")
 module.exports = (app)=>{
     app.get("/api/hello", NinjaController.sayHello)
     //more routes here
+
+    //to get all of something
     app.get("/api/ninjas", NinjaController.findAllNinjas)
 
+
+    //to create something new
     app.post("/api/ninjas", NinjaController.createNewNinja)
 
     //find a random ninja
     app.get("/api/ninjas/random", NinjaController.findRandomNinja)
 
+    //get one of something
     app.get("/api/ninjas/:id", NinjaController.findOneNinja)
 
+    //update one of something
     app.put("/api/ninjas/:id", NinjaController.updateNinja)
     
+    //delete one of something
     app.delete("/api/ninjas/:id", NinjaController.deleteNinja)
     
     
