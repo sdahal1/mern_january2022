3502409c163fc756ed516563aee6ce20d2f6d473
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-01-25 09:45:57 -0800

    mongoose set up models

--- 08-mongodb_mongoose/01-ninjas_belt_reviewer/client/src/App.js
+++ 08-mongodb_mongoose/01-ninjas_belt_reviewer/client/src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import NewNinjaForm from './components/NewNinjaForm';
+import {
+  BrowserRouter, //tells the application we can enable routing
+  Switch,
+  Route,
+  Link 
+} from "react-router-dom";
+
+
+/*
+
+when form submits, send post request using axios with teh form info to create something new
+*/
 
 function App() {
   return (
-    <div className="App container">
-      <h1>Ninjas Belt Reviewer</h1>
-      <NewNinjaForm></NewNinjaForm>
-    </div>
+    <BrowserRouter>
+      <div className="App container">
+        <h1>Ninjas Belt Reviewer</h1>
+        <Switch>
+          <Route exact path= "/">
+            <NewNinjaForm></NewNinjaForm>
+          </Route>
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- 08-mongodb_mongoose/01-ninjas_belt_reviewer/client/src/components/NewNinjaForm.jsx
+++ 08-mongodb_mongoose/01-ninjas_belt_reviewer/client/src/components/NewNinjaForm.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 const NewNinjaForm = () => {
+
+    //state variables for each info collected from form
+    let [firstName, setFirstName] = useState("")
+    let [lastName, setLastName] = useState("")
+    let [numBelts, setNumBelts] = useState(0)
+    let [isVeteran, setIsVeteran] = useState(false)
+
+    const createNinjaSubmitHandler = (e)=>{
+        e.preventDefault();
+        console.log(firstName, lastName, numBelts, isVeteran)
+
+        //put the info from form into an object
+        let formInfoObj = {firstName, lastName, numBelts, isVeteran};
+
+        axios.post("http://localhost:8000/api/ninjas", formInfoObj)
+            .then(res=>{
+                console.log("response after posting", res)
+            })
+            .catch(err=>console.log("error in submitting post request",err))  
+
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit = {createNinjaSubmitHandler}>
                 <div className="form-group">
                     <label htmlFor="">First Name</label>
-                    <input type="text" name="" id="" className="form-control" />
+                    <input onChange = {(e)=>{setFirstName(e.target.value)}} type="text" name="" id="" className="form-control" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Last Name</label>
-                    <input type="text" name="" id="" className="form-control" />
+                    <input onChange = {(e)=>{setLastName(e.target.value)}} type="text" name="" id="" className="form-control" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Number of Belts</label>
-                    <input type="number" name="" id="" className="form-control" />
+                    <input onChange = {(e)=>{setNumBelts(e.target.value)}} type="number" name="" id="" className="form-control" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Is Ninja a Veteran?</label>
-                    <input type="checkbox" name="" id="" className="form-checkbox" />
+                    <input onChange = {(e)=>{setIsVeteran(e.target.checked)}} type="checkbox" name="" id="" className="form-checkbox" />
                 </div>
                 <input type="submit" value="Create Ninja!" className="btn btn-success mt-3" />
             </form>
