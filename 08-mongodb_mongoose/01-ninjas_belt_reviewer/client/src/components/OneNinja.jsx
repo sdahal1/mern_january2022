b4d57e5ae847254ee7beaa7b1b90936cf71accd8
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-01-25 14:21:23 -0800

    mongoose set up models

--- 08-mongodb_mongoose/01-ninjas_belt_reviewer/client/src/components/OneNinja.jsx
+++ 08-mongodb_mongoose/01-ninjas_belt_reviewer/client/src/components/OneNinja.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from "react-router";
+import axios from 'axios';
 
 const OneNinja = () => {
     const { id } = useParams();
 
+    const [ninjaDetails, setNinjaDetails] = useState({})
+
+    useEffect(()=>{
+        axios.get(`http://localhost:8000/api/ninjas/${id}`)
+        .then(res=>{
+            console.log("response when making request for one ninja", res)
+            //save res.data.results into state
+            setNinjaDetails(res.data.results)
+        })
+        .catch(err=> console.log(err))
+    }, [])
+    
+
     return (
         <div>
-            <h1>Details about One Ninja</h1>
+            <h4>Details about {ninjaDetails.firstName} {ninjaDetails.lastName}</h4>
+            <p>Number of belts: {ninjaDetails.numBelts}</p>
             <p>ID: {id}</p>
+            <p>Veteran status: {ninjaDetails.isVeteran? "Is a Veteran": "Not a Veteran"}</p>
         </div>
     );
 };
