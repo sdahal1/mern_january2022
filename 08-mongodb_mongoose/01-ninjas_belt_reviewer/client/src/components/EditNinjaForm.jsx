0100595351cae5ce18bbc940e665e2f60c2cb7c9
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-01-26 08:56:46 -0800

    mongoose set up models

--- 08-mongodb_mongoose/01-ninjas_belt_reviewer/client/src/components/EditNinjaForm.jsx
+++ 08-mongodb_mongoose/01-ninjas_belt_reviewer/client/src/components/EditNinjaForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from "react-router";
 import axios from 'axios';
 
@@ -13,6 +13,23 @@ const EditNinjaForm = () => {
 
     //get the route param info containing the id of the object we want to edit
     const { id } = useParams();
+
+    //state variable to save the info i get back from api about one ninja (or one object)
+    let [ninjaInfo, setNinjaInfo] = useState({})
+
+
+    useEffect(()=>{
+        //make an axios call to get information from the backend about one ninja using the api endpoint to get one ninja
+        axios.get(`http://localhost:8000/api/ninjas/${id}`)
+        .then(res=>{
+            console.log("response is this-->", res)
+            setNinjaInfo(res.data.results)
+
+        })
+        .catch(err=> console.log(err))
+    },[])
+    
+
 
     const createNinjaSubmitHandler = (e)=>{
         e.preventDefault();
@@ -35,19 +52,19 @@ const EditNinjaForm = () => {
             <form onSubmit = {createNinjaSubmitHandler}>
                 <div className="form-group">
                     <label htmlFor="">First Name</label>
-                    <input onChange = {(e)=>{setFirstName(e.target.value)}} type="text" name="" id="" className="form-control" />
+                    <input onChange = {(e)=>{setFirstName(e.target.value)}} type="text" name="" id="" className="form-control" value={ninjaInfo.firstName}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Last Name</label>
-                    <input onChange = {(e)=>{setLastName(e.target.value)}} type="text" name="" id="" className="form-control" />
+                    <input onChange = {(e)=>{setLastName(e.target.value)}} type="text" name="" id="" className="form-control" value={ninjaInfo.lastName} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Number of Belts</label>
-                    <input onChange = {(e)=>{setNumBelts(e.target.value)}} type="number" name="" id="" className="form-control" />
+                    <input onChange = {(e)=>{setNumBelts(e.target.value)}} type="number" name="" id="" className="form-control" value={ninjaInfo.numBelts} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Is Ninja a Veteran?</label>
-                    <input onChange = {(e)=>{setIsVeteran(e.target.checked)}} type="checkbox" name="" id="" className="form-checkbox" />
+                    <input onChange = {(e)=>{setIsVeteran(e.target.checked)}} type="checkbox" name="" id="" className="form-checkbox" checked={ninjaInfo.isVeteran} />
                 </div>
                 <input type="submit" value="Update Ninja!" className="btn btn-success mt-3" />
             </form>
