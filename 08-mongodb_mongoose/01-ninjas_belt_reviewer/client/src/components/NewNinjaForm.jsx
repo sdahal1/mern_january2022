c20c56a054a60ef1406db610a0611f6fb0a47648
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-01-26 10:28:25 -0800

    finshed belt reviewer

--- 08-mongodb_mongoose/01-ninjas_belt_reviewer/client/src/components/NewNinjaForm.jsx
+++ 08-mongodb_mongoose/01-ninjas_belt_reviewer/client/src/components/NewNinjaForm.jsx
@@ -6,8 +6,10 @@ const NewNinjaForm = () => {
     //state variables for each info collected from form
     let [firstName, setFirstName] = useState("")
     let [lastName, setLastName] = useState("")
-    let [numBelts, setNumBelts] = useState(0)
+    let [numBelts, setNumBelts] = useState(null)
     let [isVeteran, setIsVeteran] = useState(false)
+
+    let [formErrors, setFormErrors] = useState({})
 
     const createNinjaSubmitHandler = (e)=>{
         e.preventDefault();
@@ -19,6 +21,13 @@ const NewNinjaForm = () => {
         axios.post("http://localhost:8000/api/ninjas", formInfoObj)
             .then(res=>{
                 console.log("response after posting", res)
+
+                //if the res.data.results key is there, then form validations were valid
+                //if the res.data.error key is there, then form was not filled out properly 
+                if(res.data.error){ //validation errors happened
+                    //res.data.error.errors contains an object that has my validation error messages for each input
+                    setFormErrors(res.data.error.errors)
+                }
             })
             .catch(err=>console.log("error in submitting post request",err))  
 
@@ -30,14 +39,20 @@ const NewNinjaForm = () => {
                 <div className="form-group">
                     <label htmlFor="">First Name</label>
                     <input onChange = {(e)=>{setFirstName(e.target.value)}} type="text" name="" id="" className="form-control" />
+                    <p className="text-danger">{formErrors.firstName?.message}</p>
+                    {/* <p className="text-danger">{formErrors.firstName? formErrors.firstName.message : ""}</p> */}
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Last Name</label>
                     <input onChange = {(e)=>{setLastName(e.target.value)}} type="text" name="" id="" className="form-control" />
+                    <p className="text-danger">{formErrors.lastName?.message}</p>
+
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Number of Belts</label>
                     <input onChange = {(e)=>{setNumBelts(e.target.value)}} type="number" name="" id="" className="form-control" />
+                    <p className="text-danger">{formErrors.numBelts?.message}</p>
+
                 </div>
                 <div className="form-group">
                     <label htmlFor="">Is Ninja a Veteran?</label>
